70708125a851e9497a4d4fd7d5693268af229140
Danyil <dartomov@gmail.com>
2024-09-27 17:17:45 +0200

    Fix annotations

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
+from rest_framework.request import Request
 from rest_framework.decorators import api_view
 
 from cinema.models import Movie
@@ -8,7 +9,7 @@ from cinema.serializers import MovieSerializer
 
 
 @api_view(["GET", "POST"])
-def movie_list(request):
+def movie_list(request: Request):
     if request.method == "GET":
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
@@ -21,7 +22,7 @@ def movie_list(request):
 
 
 @api_view(["GET", "PUT", "DELETE"])
-def movie_detail(request, pk):
+def movie_detail(request: Request, pk: int):
     bus = get_object_or_404(Movie, pk=pk)
     if request.method == "GET":
         serializer = MovieSerializer(bus)
